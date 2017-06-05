1acfba535de0e30243f012548823797875bbf1dd
ppbppb001 <ppb_002@yahoo.com>
2017-06-06 09:10:22 +1000

    Update Call_Python_from_R.r

--- R/Archive/R_Coding_Common_Usage/Call_Python_from_R.r
+++ R/Archive/R_Coding_Common_Usage/Call_Python_from_R.r
@@ -15,9 +15,3 @@ system('python python_func.py')
 dfz<-read.csv('output.csv')
 # print.data.frame(dfz)
 
-
-x1 <- c(1,2,3,4)
-x2 <- c(5,6,7,8)
-df <- cbind(x1, x2)
-
-system('python python_func.py')
