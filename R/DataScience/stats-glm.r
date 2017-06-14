ec42edb79d2a16130f54498e53442aad49efb1ce
ppbppb001 <ppb_002@yahoo.com>
2017-06-14 21:39:15 +1000

    Create stats-glm.r

--- R/DataScience/stats-glm.r
+++ R/DataScience/stats-glm.r
@@ -4,11 +4,11 @@
 #   package {stats}
 #=================================
 
-#library(caret)
+library(caret)
 
 # [Data Set 1]:
 # Prepare data set  .......................
-dlen <- 1000
+dlen <- 10000
 
 c1 <- sample(dlen)
 c1
@@ -35,14 +35,6 @@ my.formula <- "Class~."
 
 # Prepare refrence data:
 cm.ref <- testing$Class
-cm.ref <- as.factor(cm.ref)
-if (levels(cm.ref)[1] == "0") {
-    levels(cm.ref)[2] <- "1"
-} else {
-    levels(cm.ref)[2] <- "0"
-}
-
-
 
 # Train/Fit ...........................................
 set.seed(666)
@@ -52,7 +44,7 @@ my.glmfit <- glm(formula = my.formula,
                  method = "glm.fit",
                  family = binomial(link="logit"),
                  control = glm.control(epsilon = 1e08, maxit = 10, trace = FALSE)
-)
+                )
 print(my.glmfit)
 
 
@@ -60,28 +52,33 @@ print(my.glmfit)
 my.glmpred <- predict(object = my.glmfit,
                       newdata = testing,
                       type = "response"
-)
+                      )
 print(my.glmpred)
 
 
 # confusion maxtrix: ...................................
 # Prepare prediction data for confusion matrix:
 cm.pred <- ifelse(my.glmpred > 0.5, 1, 0)  # if pred > 0.5 then 1 else 0
-cm.pred <- as.factor(cm.pred)
-if (levels(cm.pred)[1] == "0") {
-    levels(cm.pred)[2] <- "1"
-} else {
-    levels(cm.pred)[2] <- "0"
-}
-print(cm.pred)   # Prediction
 
-print(cm.ref)    # Reference
+# prediction data to factor:
+cm.pred <- c(0, 1, cm.pred)                # Add preamble (0,1)
+cm.pred <- as.factor(cm.pred)              # Convert to factor
+cm.pred <- cm.pred[-c(1,2)]                # Remove preamble
+print(head(cm.pred)) 
 
-#my.cm <- confusionMatrix(data = cm.pred,
-#                         reference = cm.ref,
-#                         mode = "prec_recall"
-#)
-#print(my.cm)     # output of confusion matrix
+# reference data to factor:
+cm.ref <- c(0, 1, cm.ref)                  # Add preamble (0,1)
+cm.ref <- as.factor(cm.ref)                # Convert to factor
+cm.ref <- cm.ref[-c(1,2)]                  # Remove preamble
+print(head(cm.ref))
+
+my.cm <- confusionMatrix(data = cm.pred,
+                         reference = cm.ref,
+                         mode = "prec_recall"
+                        )
+print(my.cm)     # output of confusion matrix
 
 my.table <- table(cm.ref, cm.pred)
 print (my.table)
+
+
