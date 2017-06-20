baef1768fd103ad5f40c5fb4b107d54205b1c9bc
ppbppb001 <ppb_002@yahoo.com>
2017-06-20 21:23:34 +1000

    Update ggplot2-mytools-lib.r

--- R/Visualization/ggplot2-mytools-lib.r
+++ R/Visualization/ggplot2-mytools-lib.r
@@ -2,7 +2,8 @@
 #
 #   Chart plotting tools for specified purposes
 #
-#   [2017-06-16]
+#   [2017-06-16] - Initialized
+#   [2017-06-20] - multiPlot() added
 #-----------------------------------------------
 
 
@@ -18,7 +19,7 @@ plotLines <- function(title = NULL,
                       x.name="X", y.name="Y",
                       x.values=NULL, y.values=NULL,
                       x.range=c(0,1), y.range=c(0,1),
-                      index.text = NULL, index.pos = 4) {
+                      index.text = NULL, index.pos = 4, index.x = 0.75) {
     
     # Check input values:
     pcnt <- length(x.values)
@@ -40,7 +41,7 @@ plotLines <- function(title = NULL,
                          index.text)
 
     # basic plot
-    ggp <- ggplot() +
+    ggp <- ggplot(data.frame(Group=index.text)) +
             labs(x = x.name, y = y.name, title = title) +
             xlim(x.range[1], x.range[2]) + 
             ylim(y.range[1], y.range[2])
@@ -74,7 +75,8 @@ plotLines <- function(title = NULL,
         if (bitwAnd(index.pos-1, 0x02) == 0) {
             t.x1 <- x.range[1]
         } else {
-            t.x1 <- x.range[2]
+            # t.x1 <- x.range[2]
+            t.x1 <- (x.range[2] - x.range[1])*index.x
         }
         if (bitwAnd(index.pos-1, 0x01) == 0) {
             t.y1 <- y.range[2] 
@@ -92,8 +94,8 @@ plotLines <- function(title = NULL,
             geom_text(
                 data = df.index,
                 aes(x = X, y = Y, label = Index, color = Index),
-                hjust = "inward", 
-                family = "mono", fontface = "bold"
+                hjust = "left"
+                # family = "mono", fontface = "bold"
             )
     }
     
@@ -154,4 +156,54 @@ plotPoints <- function(title = NULL,
     }
     return(ggp)    
 }
+
+
+
+
+# Multiple plot function
+#
+# ggplot objects can be passed in ..., or to plotlist (as a list of ggplot objects)
+# - cols:   Number of columns in layout
+# - layout: A matrix specifying the layout. If present, 'cols' is ignored.
+#
+# If the layout is something like matrix(c(1,2,3,3), nrow=2, byrow=TRUE),
+# then plot 1 will go in the upper left, 2 will go in the upper right, and
+# 3 will go all the way across the bottom.
+#
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+    library(grid)
+    
+    # Make a list from the ... arguments and plotlist
+    plots <- c(list(...), plotlist)
+    
+    numPlots = length(plots)
+    
+    # If layout is NULL, then use 'cols' to determine layout
+    if (is.null(layout)) {
+        # Make the panel
+        # ncol: Number of columns of plots
+        # nrow: Number of rows needed, calculated from # of cols
+        layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                         ncol = cols, nrow = ceiling(numPlots/cols))
+    }
+    
+    if (numPlots==1) {
+        print(plots[[1]])
+        
+    } else {
+        # Set up the page
+        grid.newpage()
+        pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+        
+        # Make each plot, in the correct location
+        for (i in 1:numPlots) {
+            # Get the i,j matrix positions of the regions that contain this subplot
+            matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+            
+            print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                            layout.pos.col = matchidx$col))
+        }
+    }
+}
+
 
