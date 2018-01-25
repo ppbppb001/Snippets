9619f8ee080f6b3677e2e13a2798428ed96ef893
ppbppb001 <ppb_002@yahoo.com>
2018-01-25 13:23:11 +1100

    Add files via upload

--- Python/MiniProject/pick-by-matching.py
+++ Python/MiniProject/pick-by-matching.py
@@ -16,33 +16,32 @@ import pandas as pd
 
 
 # Make pseudo data set
-code_low = 1000
-code_high = 4000
+code_low = 0
+code_high = 9999
 dataset_size = 100000
 
-codes = ['%s'%(x) for x in range(code_low, code_high)]   # list of codes
-# dfCode = pd.DataFrame(data=codes, columns=["Code"])
+codes = np.random.randint(low=code_low+2000, 
+                          high=code_high-3000, 
+                          size=dataset_size)  # subset of [code_low:code_high]
+values = np.random.randint(low=1, 
+                           high=dataset_size*100, 
+                           size=dataset_size)
+tags = ['Tag string is <Code=%4.4d, Tag=%8.8d>'%(x[0], x[1]) for x in zip(codes,values)] # create tag strings
 
-data_codes = np.random.randint(low=code_low+100, 
-                               high=code_high, 
-                               size=dataset_size)  # 100items less than 'codes'
-data_tagcodes = np.random.randint(low=1, 
-                                  high=dataset_size*100, 
-                                  size=dataset_size)
-data_tags = ['Tag string is <Code=%4.4d, Tag=%8.8d>'%(x[0], x[1]) for x in zip(data_codes,data_tagcodes)] # create tag strings
-
-dfData = pd.DataFrame(data=zip(data_codes, data_tags),
-                      columns=["Code", "Tag"])          # The pseudo data set
+dfData = pd.DataFrame(data=zip(codes, tags),columns=["Code", "Tag"])          # The pseudo data set
 dfData
 
 
 # In[3]:
 
 
+# Make sub-dataframes by matching the code:
+
 tk1 = tm.clock()  # for benchmark
 
-result = []  # list to store the sub-dataframe for each code
-for code in codes:
+codeTable = set(dfData["Code"].values)  # code index retrieved from dfData
+result = []   # list to store the sub-dataframe for each code
+for code in codeTable:
     chk = dfData['Code'].isin([code])
     dfSub = dfData[chk]
     if len(dfSub) > 0:
@@ -69,5 +68,5 @@ print "result[1000]: rows=%d\n"%(len(dfX)),dfX.head()
 # In[5]:
 
 
-result[100]
+result[200]
 
