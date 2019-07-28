388f6c5b61a5e73758cc6f87bbffe56c04297ec3
ppbppb001 <ppb_002@yahoo.com>
2019-07-28 16:35:14 +1000

    Add files via upload

---
+++ R/Html/parse-html-tables.R
@@ -0,0 +1,272 @@
+# ***********************************************
+#
+#  Functions
+#
+# ***********************************************
+
+# ---Function: Load the predefined column names from file
+loadColNames <- function(filename = NULL){
+  fn <- filename
+  fr <- file(fn, "r")
+  
+  output <- list()
+  len <- 1
+  while (len > 0) {
+    ln <- readLines(fr, n=1)
+    len <- length(ln)
+    if (len > 0){
+      if (nchar(ln)>0){
+        p <- regexpr("#", ln, ignore.case = TRUE)
+        if (p[[1]][1] != 1) {
+          output <- c(output, ln)
+        }
+      }
+    }
+  }
+  
+  close(fr)
+  return(output)
+}
+
+
+# ---Function: Load the file which hold the list of data files in html format:
+loadHtmlFileNames <- function (filename=NULL){
+  output <- list()  
+  print(filename)
+  
+  # fr <- file(filename,"r",encoding = "UTF-16")
+  # fr <- file(filename,"r",encoding = "UTF-8")
+  fr <- file(filename,"r")
+  lns <- readLines(fr,n=1000000)
+  close(fr)
+  
+  lns <- trimws(lns)
+  count <- length(lns)
+  for (i in 1:count){
+    n <- grep("\\.HTML", lns[i], ignore.case = TRUE)
+    if (length(n)>0){
+      s <- strsplit(lns[i], " ")
+      l = length(s[[1]])
+      ss <- s[[1]][l]
+      output <- c(output, ss)
+    }
+  }
+  return (output)
+}
+
+
+# ---Function: extract text body between the tag pair
+htmlExtractTaggedText <- function(input = NULL,
+                               tag1 = NULL,
+                               tag2 = NULL) {
+  output <- list()
+  
+  p1 <- gregexpr(pattern = tag1, input)
+  if (p1[[1]][1] < 1){
+    return (output)
+  }
+  p2 <- gregexpr(pattern = tag2, input)
+  if (p2[[1]][1] < 1){
+    return(output)
+  }
+  
+  count <- length(p1[[1]])
+  for (i in 1:count) {
+    ix1 <- p1[[1]][i]
+    ix2 <- p2[[1]][i]
+    s <- substr(input, ix1, ix2-1)
+    x1 <- gregexpr(pattern = ">", s)
+    if (x1[[1]][1] > 0){
+      s <- substr(s, x1[[1]][1]+1, nchar(s))
+      output <- c(output, list(s))
+    }
+  }
+  
+  return(output)
+}
+
+
+# ---Function: extract tables from html text data
+htmlExtractTables <- function(input = NULL) {
+  tag1 <- "<table"
+  tag2 <- "</table>"
+  output <- htmlExtractTaggedText(input, tag1, tag2)
+  return(output)
+}
+
+
+# ---Function: extract rows from the table part
+htmlExtractRows <- function(input = NULL) {
+  tag1 <- "<tr"
+  tag2 <- "</tr>"
+  output <- htmlExtractTaggedText(input, tag1, tag2)
+  return(output)
+}
+
+
+#---Function: extract columns from the row part
+htmlExtractCols <- function(input = NULL) {
+  tag1 <- "<td"
+  tag2 <- "</td>"
+  output <- htmlExtractTaggedText(input, tag1, tag2)
+  print (output)
+  if (length(output) < 1){
+    tag1 <- "<th"
+    tag2 <- "</th>"
+    output <- htmlExtractTaggedText(input, tag1, tag2)
+    print ('using TH')
+  }
+  return(output)
+}
+
+
+#--- Function: extract data from the html table
+htmlExtractData <- function(input = NULL){
+  output <- list()
+  
+  tbs <- htmlExtractTables(input)
+  if (length(tbs) < 1){
+    # stop("Failed to parse html file to extract tabel(s).")
+    return (output)
+  }
+
+  for (i in 1:length(tbs)){
+  # for (i in 1:1){
+    tb <- tbs[[i]]
+    rows <- htmlExtractRows(tb)
+    # Process the table which have more than 2 rows
+    if (length(rows) > 1){  
+      cols1 <- htmlExtractCols(rows[[1]])
+      cols2 <- htmlExtractCols(rows[[2]])
+      if (length(cols1)>0 && length(cols2)>0 && length(cols1)==length(cols2)){
+        output$name <- c(output$name, cols1)
+        output$value <- c(output$value, cols2)
+      }
+    }
+  }
+  return(output)
+}
+
+
+#--- Function: create a one row data from the html parsing output
+makeDataFrameFromHTML <- function(input=NULL, names=NULL){
+  data <- htmlExtractData(input)
+  
+  in.name <- data$name
+  in.value <- data$value
+  if (length(in.name) < 1){
+    return(NULL)
+  }
+  
+  # Make an empty data frame from 'colNames'
+  cells <- rep("", length=length(names))
+  df <- data.frame(as.list(cells), stringsAsFactors = FALSE)
+  colnames(df) <- make.names(names)
+  
+  ixb <- in.name %in% names
+  matchedName <- in.name[ixb]
+  matchedValue <- in.value[ixb]
+  
+  
+  for (i in 1:length(names)){
+    ix <- which(matchedName %in% names[[i]])
+    df[1,i] <- as.character(matchedValue[[ix]])
+  }
+  
+  return(df)
+}
+
+
+
+
+
+
+
+# ***********************************************
+#
+#  <<< Main entry of the code >>>
+#
+#  Retrieve data from the html tables
+#
+# ***********************************************
+
+# Configurations:
+result.fn <- "result.csv"                   # save result data frame to this file
+
+colNames.fn <- "colnames.txt"               # definitions of column name
+
+htmlFiles.folder <- ".\\htmlfiles_Test\\"   # the folder where html data files are stored
+
+htmlFiles.fn <- "htmlfiles.txt"             # name list of the html data files
+#
+# *** How to generate name list of the html files ***
+#
+# [Approach-1]:
+#    - Open Powershell console
+#    - Enter the folder where the html data files are stored
+#    - type command as following: 
+#        dir *.html | out-file -encoding ASCII htmlfiles.txt
+#
+# [Approach-2]:
+#    - Open Powershell console
+#    - Enter the folder where the html data files are stored
+#    - Copy the 'list.bat' batch file from the 'htmlfiles_test' folder
+#    - Run 'list.bat' batch file by typing command as following:
+#        .\list.bat
+#
+
+
+# Benchmark
+tmStart <- proc.time()[3]
+
+
+# Load predefined column names 
+colNames.list <- loadColNames(colNames.fn)
+if (length(colNames.list) < 1){
+  stop("Failed to load definitions of column name")
+}
+colNames.count <- length(colNames.list)
+
+
+# Load name list of the html data files
+htmlFiles.fileNames <- loadHtmlFileNames(paste(htmlFiles.folder, htmlFiles.fn, sep=""))
+htmlFiles.count <- length(htmlFiles.fileNames)
+if (htmlFiles.count < 1) {
+  stop("<<<STOP>>> No html file to process!")
+}
+
+
+# Make data frame from the html files
+result.df <- NULL
+
+for (i in 1:length(htmlFiles.fileNames)){
+  fn <- paste(htmlFiles.folder, htmlFiles.fileNames[[i]], sep = "")
+  fr<-file(fn,"rb")
+  htmltext <- rawToChar(readBin(fr,what="raw",n=1000000,1))
+  close(fr)
+  
+  df <- makeDataFrameFromHTML(htmltext, colNames.list)
+  if (is.null(result.df)){
+    result.df <- df
+  } else {
+    result.df <- rbind(result.df, df)
+  }
+}
+
+# Check the result
+if (is.null(result.df) || nrow(result.df) < 1){
+  Stop("<<<STOP>>> Nothing retrieved! STOP!!!")
+}
+
+
+# Benchmark
+tmFinish <- proc.time()[3]
+cat(">>> Time consumed =", (tmFinish - tmStart), "(s)")
+
+
+# Save data frame to file:
+write.csv(result.df, file = result.fn, row.names = FALSE)
+
+
+# CHECK by reading back
+dfcheck <- read.csv(result.fn, header = TRUE, stringsAsFactors = FALSE)
