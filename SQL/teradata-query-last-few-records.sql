c3931b7b1f9bdee955b1d7d1bb72fbcdccd21029
ppbppb001 <ppb_002@yahoo.com>
2019-08-15 13:51:32 +1000

    Add files via upload

--- SQL/teradata-query-last-few-records.sql
+++ SQL/teradata-query-last-few-records.sql
@@ -1,10 +1,16 @@
--- Get row count --
+/* Get row count */
 select count(1)   				/* total non-null rows in column-1 */
 from bear.timeseries as bts;
 
 
--- Get last 10 records ---
+/* Get last 10 records - descending result*/
 select *
+from bear.timeseries
+qualify rank() over(order by datex DESC, timex DESC) <= 10;  /* get ranks top 10 after descending sort by datex/timex */
+
+
+/* Get last 10 records - ascending result*/
+select * order by datex ASC, timex ASC
 from bear.timeseries
 qualify rank() over(order by datex DESC, timex DESC) <= 10;  /* get ranks top 10 after descending sort by datex/timex */
 
@@ -13,3 +19,5 @@ qualify rank() over(order by datex DESC, timex DESC) <= 10;  /* get ranks top 10
 select *
 from bear.timeseries
 qualify rank() over(order by datex DESC, timex DESC) < ((select count(1) from bear.timeseries) * 0.1);
+
+
