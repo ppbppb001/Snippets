19eed858c19d6a0c9f822d9032e4cd31d593449b
ppbppb001 <ppb_002@yahoo.com>
2019-08-15 14:01:08 +1000

    Add files via upload

--- SQL/teradata-query-last-few-records.sql
+++ SQL/teradata-query-last-few-records.sql
@@ -8,7 +8,7 @@ select *
 from bear.timeseries
 qualify rank() over(order by datex DESC, timex DESC) <= 10;  /* get ranks top 10 after descending sort by datex/timex */
 
-/* Get last 10 records - descending result - simpler verion*/
+/* Get last 10 records - descending result - simpler approach*/
 select *
 from bear.timeseries
 qualify rank(datex, timex) <= 10;  /* get ranks top 10 after descending sort by datex/timex */
@@ -18,7 +18,7 @@ select * order by datex ASC, timex ASC
 from bear.timeseries
 qualify rank() over(order by datex DESC, timex DESC) <= 10;  /* get ranks top 10 after descending sort by datex/timex */
 
-/* Get last 10 records - ascending result - simpler version*/
+/* Get last 10 records - ascending result - simpler approach*/
 select * order by datex ASC, timex ASC
 from bear.timeseries
 qualify rank(datex, timex) <= 10;  /* get ranks top 10 after descending sort by datex/timex */
@@ -29,7 +29,7 @@ select *
 from bear.timeseries
 qualify rank() over(order by datex DESC, timex DESC) <= ((select count(1) from bear.timeseries) * 0.1);
 
-/* get last 10% of records - descending result - simpler version*/
+/* get last 10% of records - descending result - simpler approach*/
 select *
 from bear.timeseries
 qualify rank(datex, timex) <= ((select count(1) from bear.timeseries) * 0.1);
