1f06a0ed826e5407a98592c9d4135b38e8f587a5
ppbppb001 <ppb_002@yahoo.com>
2019-08-25 19:38:03 +1000

    Add files via upload

--- R/Visualization/ggplot2-multiple-density-histogram.R
+++ R/Visualization/ggplot2-multiple-density-histogram.R
@@ -7,11 +7,11 @@ stdseq <- 1:20
 
 d1 <- sample(stdseq, 1000, replace = TRUE)
 data1 <- data.frame(X = d1)                       # pseudo source data frame 1
-df1 <- data.frame(X = data1[,"X"], SET="R112")     # df1 = data frame composed for plotting
+df1 <- data.frame(X = data1[,"X"], SET = "R112")     # df1 = data frame composed for plotting
 
 d2 <- sample(stdseq, 10000, replace = TRUE)
 data2 <- data.frame(X = d2)                       # data2 = pseudo source data frame 2 
-df2 <- data.frame(X = data2[,"X"], SET="V231")    # df2 = data frame composed for plotting
+df2 <- data.frame(X = data2[,"X"], SET = "V231")    # df2 = data frame composed for plotting
 
 g1 <- ggplot(data = dumb, aes(x=X, fill = SET)) +
         geom_density(data = df1, alpha = 0.7) +
