3ad9fbf76ffc003717a20190bb2cf891c59235b4
ppbppb001 <ppb_002@yahoo.com>
2020-08-04 16:11:15 +1000

    Add files via upload

--- Graph/Gremlin/test-py_req_gremlin.R
+++ Graph/Gremlin/test-py_req_gremlin.R
@@ -5,7 +5,7 @@ script <- '"d:\\user\\studyandtest\\r\\github.com\\graph\\py_requests_gremlin.py
 url <- '--url http://192.168.1.52:8182'
 auth <- '--auth user1234:pass1234'
 gremlin <- '--gremlin g.V().range(0,10).count()'
-# outfile <- '--outfile d:\\user\\studyandtest\\r\\rtest-1\\res.txt'
+# outfile <- '--outfile d:\\user\\studyandtest\\r\\github.com\\graph\\res.txt'
 outfile <- ''
 
 cmd <- paste0(py,' ',
